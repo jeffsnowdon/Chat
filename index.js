44bdca0a3f1e80d8511aed20ed2a02aa5225a667
Jeffrey Snowdon <jeffsnowdon@gmail.com>
2016-12-11 14:42:02 -0500

    Add connect/disconnect notifications. Refactor style into its own file. Refactor browser-side js into its own file.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-  console.log('a user connected');
+  io.emit('user connected', 'a user connected');
   socket.on('disconnect', function () {
-    console.log('user disconnected');
+    io.emit('user disconnected', 'a user disconnected');
   });
   socket.on('chat message', function (msg) {
     io.emit('chat message', msg);
